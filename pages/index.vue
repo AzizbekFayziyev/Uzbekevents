695b055e3bff5a7ff3c088134016a2f673b6e958
AzizbekFayziyev <fayziyev.dev@gmail.com>
2024-10-05 20:12:15 +0500

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -23,13 +23,13 @@ useHead({
 		{ name: 'og:title', content: 'Всего одним нажатием Поделитесь своей контактной информацией:' },
 		{ name: 'description', content: 'Создайте бренд, позиционируйте себя и отличайтесь от других с первой точки контакта' },
 		{ name: 'og:description', content: 'Создайте бренд, позиционируйте себя и отличайтесь от других с первой точки контакта' },
-		{ name: 'og:image', content: '/assets/images/Group.png' },
+		{ name: 'og:image', content: '/Group.png' },
 		{ name: 'og:image:width', content: '1200' },
 		{ name: 'og:image:height', content: '630' },
 		{ name: 'og:type', content: 'website' },
 		{ name: 'og:url', content: `https://uzbekbusinessconnect.com/` },
 		{ name: 'twitter:card', content: 'summary_large_image' },
-		{ name: 'twitter:image', content: '/assets/images/Group.png' },
+		{ name: 'twitter:image', content: '/Group.png' },
 	],
 	link: [
 		{ rel: 'icon', type: 'image/x-icon', href: '/logo.png' }
